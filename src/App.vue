a8cee27dca03187702dd47660a595dfd0c19f67f
Christian Engler <christian.engler@despegar.com>
2021-02-15 18:25:09 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Traffic msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Traffic from './components/Traffic.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Traffic
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+#app {
+  margin: 0px;
+}
+</style>
---
+++ src/components/Traffic.vue
@@ -0,0 +1,126 @@
+<template>
+  <div ref="chartdiv" id="chartdiv"></div>
+</template>
+
+<script>
+import ForceGraph3D from '3d-force-graph'
+import SpriteText from 'three-spritetext'
+import * as THREE from 'three'
+import * as UnrealBloomPass from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+//import * as BokehPass from 'three/examples/jsm/postprocessing/BokehPass'
+//import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader.js';
+
+import _ from 'lodash'
+
+let links = [{"from":"internet","to":"home","value":0,"prom":0.0}, {"from":"home","to":"search","value":10,"prom":1000.0}]
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      nodes: null
+    }
+  },
+  mounted() {
+    // Random tree
+
+    //links = links.filter(l => l.value > 20)
+    const GROUPS = 12;
+    let nodes = links.map(i => ({
+      id: i.from,
+      color: Math.round(Math.random() * Math.pow(2, 24)),
+      group: Math.ceil(Math.random() * GROUPS) }))
+    nodes = nodes.concat(links.map(i => ({ id: i.to })))
+    nodes = _.uniqBy(nodes, 'id')
+
+    console.log(nodes.map(i => i.id))
+
+    const gData = {
+      nodes: nodes,
+      links: links
+          .filter(id => id)
+          .map(id => ({
+            source: id.from,
+            target: id.to,
+            curvature: 0.3,
+            value: id.value/10,
+            prom: id.prom,
+            color: nodes[_.findIndex(nodes, {id: id.from})].color
+          }))
+    };
+
+    this.nodes = gData
+
+    const myGraph = ForceGraph3D();
+    myGraph(document.getElementById("chartdiv"))
+        .nodeThreeObject(node => {
+          let group = new THREE.Group();
+
+          let mesh = new THREE.Mesh(
+            new THREE.SphereGeometry(4),
+            new THREE.MeshLambertMaterial({
+              color: node.color,
+              transparent: true,
+              opacity: 0.76
+            }))
+
+          //mesh.position.set( 0, 0, 0 );
+          group.add(mesh)
+
+          const sprite = new SpriteText(node.id);
+          sprite.material.depthWrite = false; // make sprite background transparent
+          sprite.textHeight = 4;
+          sprite.position.setY(-8);
+
+
+          group.add(sprite)
+
+          return group
+
+        })
+        .linkColor('color')
+        .linkDirectionalParticles("value")
+        .linkCurvature('curvature')
+
+        .linkThreeObject(link => {
+          // extend link with text sprite
+          const sprite = new SpriteText(`${link.source} > ${link.target}\n ${link.value*10}rpm ${link.prom/1000}s`);
+          sprite.color = 'lightgrey';
+          sprite.textHeight = 1;
+          return sprite;
+        })
+        // eslint-disable-next-line no-unused-vars
+        .linkPositionUpdate((sprite, { start, end }, link) => {
+          Object.assign(sprite.position, link.__curve.getPoint(0.5));
+        })
+       .linkDirectionalArrowLength(3.5)
+        .linkDirectionalArrowRelPos(0)
+
+        //.linkDirectionalParticleSpeed(d => d.value * 0.0001)
+      .graphData(gData);
+
+        const bloomPass = new UnrealBloomPass.UnrealBloomPass()
+        bloomPass.strength = 1;
+        bloomPass.radius = 1;
+        bloomPass.threshold = 0.1;
+        //myGraph.postProcessingComposer().addPass(bloomPass);
+
+
+
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body { margin: 0; }
+
+.chartdiv {
+  height: 100%;
+  width: 100%;
+}
+
+</style>
